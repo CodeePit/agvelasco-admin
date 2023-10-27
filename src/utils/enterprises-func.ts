5110dfd27fd42ba9df0dea5c87751a78f369baaf
Guilherme Brogio <guilhermebrogio.ps@gmail.com>
2023-10-27 00:17:12 -0300

    Feats

--- src/utils/enterprises-func.ts
+++ src/utils/enterprises-func.ts
@@ -49,7 +49,7 @@ function returnArray(v: any) {
 }
 
 async function getMediaIfIdExists(id: any) {
-  console.log(`${id}`.length);
+  console.log(id, `${id}`?.length);
   if (!`${id}`.length) return null;
   return await getMedia(`${id}`).then((r) => r || null);
 }
