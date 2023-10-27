7d113787b8daba66ade710f799513b0c1fcae96f
Guilherme Brogio <guilhermebrogio.ps@gmail.com>
2023-10-27 01:25:29 -0300

    Feats

--- src/utils/enterprises-func.ts
+++ src/utils/enterprises-func.ts
@@ -77,11 +77,11 @@ export async function getEnterprise(id: string) {
           id: item.id,
           title: item.title.rendered,
 
-          featured_media: await getMediaIfIdExists(item.featured_media),
-          sobre_imagem: await getMediaIfIdExists(sobre_imagem),
-          video_background: await getMediaIfIdExists(video_background),
-          diferenciais_imagem: await getMediaIfIdExists(diferenciais_imagem),
-          mapa_imagem: await getMediaIfIdExists(mapa_imagem),
+          // featured_media: await getMediaIfIdExists(item.featured_media),
+          // sobre_imagem: await getMediaIfIdExists(sobre_imagem),
+          // video_background: await getMediaIfIdExists(video_background),
+          // diferenciais_imagem: await getMediaIfIdExists(diferenciais_imagem),
+          // mapa_imagem: await getMediaIfIdExists(mapa_imagem),
 
           imovel_banners: await Promise.all(
             returnArray(imovel_banners).map(async (banner: any) => ({
